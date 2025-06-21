e143e6c13e0385e8efa1a01acae5096a30220155
Dorin Hongu <dhongu@gmail.com>
2025-06-21 07:32:55 +0300

    [REF] Refactor module structure and introduce Valuation Area management

--- deltatech_stock_valuation/models/account_move.py
+++ deltatech_stock_valuation/models/account_move.py
@@ -9,7 +9,7 @@ from odoo import models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def recompute_valuation(self):
+    def _recompute_valuation(self):
         for move in self:
             for line in move.line_ids:
                 if line.product_id and line.account_id.is_for_stock_valuation:
@@ -25,25 +25,17 @@ class AccountMove(models.Model):
                     valuation_history = self.env["product.valuation.history"].get_valuation(
                         line.product_id.id, valuation_area.id, line.account_id.id, move.date, line.company_id.id
                     )
-                    valuation_history.recompute_amount()
+                    valuation_history._recompute_amount()
 
                     valuation = self.env["product.valuation"].get_valuation(
                         line.product_id.id, valuation_area.id, line.account_id.id, line.company_id.id
                     )
-                    valuation.recompute_amount()
+                    valuation._recompute_amount()
 
-    # def _post(self, soft=True):
-    #     res = super()._post(soft=soft)
-    #     self.recompute_valuation()
-    #     return res
-    #
-    # def button_draft(self):
-    #     res = super().button_draft()
-    #     self.recompute_valuation()
-    #     return res
+
 
     def write(self, vals):
         res = super().write(vals)
         if vals.get("state"):
-            self.recompute_valuation()
+            self._recompute_valuation()
         return res
