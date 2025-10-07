249a750c25e39816bf5e51ed32149dcde996783b
Dorin Hongu <dhongu@gmail.com>
2025-10-07 22:35:22 +0300

    fix

--- deltatech_valuation_area/__manifest__.py
+++ deltatech_valuation_area/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Deltatech Stock Valuation Area",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "summary": "Stock Valuation Area Management",
     "category": "Valuation/Valuation",
     "author": "Terrabit, Dorin Hongu",
