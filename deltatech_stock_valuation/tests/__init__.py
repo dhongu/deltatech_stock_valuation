c86e2821b93b9677b58ee97f29d11d3f279868f6
Dorin Hongu <dhongu@gmail.com>
2025-11-01 10:18:39 +0200

    [IMP] Actualizează metoda și adaugă teste pentru recalculare
    Înlocuit apelurile metodei `recompute_amount` cu `_recompute_amount` pentru alinierea la convențiile interne. Adăugat un test nou pentru verificarea corectitudinii recalculării în funcție de istoricul evaluărilor produselor. Completat fișierul `__init__.py` pentru a include noile teste.

--- deltatech_stock_valuation/tests/__init__.py
+++ deltatech_stock_valuation/tests/__init__.py
@@ -3,3 +3,5 @@
 # See README.rst file on addons rcoot folder for license details
 
 from . import test_stock_valuation
+from . import test_recompute_valuation
+from . import test_recompute_product_template
