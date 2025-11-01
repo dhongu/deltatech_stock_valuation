c7ed63c065f083de599f235f77d452c08bd69d9a
Dorin Hongu <dhongu@gmail.com>
2025-11-01 06:15:17 +0200

    [REM] Comentează logica pentru câmpul `standard_price`
    Marcate ca comentarii metodele și definiția câmpului `standard_price` din modelul `product_product`. Această modificare apare ca parte dintr-o posibilă refactorizare sau depanare temporară, fără a elimina definitiv codul.

--- deltatech_stock_valuation/models/product_product.py
+++ deltatech_stock_valuation/models/product_product.py
@@ -3,7 +3,7 @@
 # See README.rst file on addons root folder for license details
 
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductProduct(models.Model):
@@ -16,22 +16,22 @@ class ProductProduct(models.Model):
         help="Valuations of this product in different valuation areas and accounts.",
     )
 
-    standard_price = fields.Float(
-        company_dependent=False,
-        compute="_compute_standard_price",
-        inverse="_inverse_standard_price",
-        search="_search_standard_price",
-    )
-
-    @api.depends_context("valuation_area", "account")
-    @api.depends("company_id")
-    def _compute_standard_price(self):
-        # valuation_area = self.env.context.get("valuation_area", False)
-
-        self._fields["standard_price"]._compute_company_dependent(self)
-
-    def _inverse_standard_price(self):
-        self._fields["standard_price"]._inverse_company_dependent(self)
-
-    def _search_standard_price(self, operator, value):
-        return self._fields["standard_price"]._search_company_dependent(self, operator, value)
+    # standard_price = fields.Float(
+    #     company_dependent=False,
+    #     compute="_compute_standard_price",
+    #     inverse="_inverse_standard_price",
+    #     search="_search_standard_price",
+    # )
+    #
+    # @api.depends_context("valuation_area", "account")
+    # @api.depends("company_id")
+    # def _compute_standard_price(self):
+    #     # valuation_area = self.env.context.get("valuation_area", False)
+    #
+    #     self._fields["standard_price"]._compute_company_dependent(self)
+    #
+    # def _inverse_standard_price(self):
+    #     self._fields["standard_price"]._inverse_company_dependent(self)
+    #
+    # def _search_standard_price(self, operator, value):
+    #     return self._fields["standard_price"]._search_company_dependent(self, operator, value)
